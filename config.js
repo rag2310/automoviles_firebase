e9bf01696178305ac97a0946930064fd62634d78
Rodolfo Gutierrez <rag.ortega@gmail.com>
2017-11-11 17:51:58 +0100

    Prueba Programacion Grafica

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyA3CxPSfSRLrKctSoVljtDwOZQInN33wpI",
+    authDomain: "automoviles-1de9d.firebaseapp.com",
+    databaseURL: "https://automoviles-1de9d.firebaseio.com",
+    projectId: "automoviles-1de9d",
+    storageBucket: "automoviles-1de9d.appspot.com",
+    messagingSenderId: "507038454956"
+};
+
+module.export config
